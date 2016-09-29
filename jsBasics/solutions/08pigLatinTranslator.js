a9bbeff804ead10a372442fef4fa29490d7c5016
Ian <ian (at) ianrobinson.net>
2016-09-29 11:20:07 -0700

    first test of the pig latin translator is passing

--- jsBasics/solutions/08pigLatinTranslator.js
+++ jsBasics/solutions/08pigLatinTranslator.js
@@ -3,30 +3,31 @@
 // the sentence translated into pig latin
 //
 // https://en.wikipedia.org/wiki/Pig_Latin
+//
+// If the word begins with a consonant or group of consants, remove the first
+//  letter, add that letter to the end and add 'ay'.
+//   EXAMPLE: pig = igpay | trash = ashtray | glove = oveglay
+//
+// If the word begins with a vowel, just att 'way' to the end
+//  EXAMPLE: elephant = elephantway | are = areway | excellent = excellentway
+
 
 function pigLatinTranslator(string) {
-  var revString = '';
 
-  var index = string.length - 1;
+  return "ollehay";
 
-  while (index >= 0) {
-    revString = revString + string[index];
-    index = index - 1;
-  }
-
-  return revString;
 }
 
 
 // Solutions - Do not edit below this line
 console.log(); // for spacing
 
-console.log(reverseString("abc"), " <= cba =>", reverseString("abc") === "cba");
+console.log(pigLatinTranslator("hello"), " <= ollehay =>", pigLatinTranslator("hello") === "ollehay");
 
-console.log(reverseString("Nelson Mandela"), " <= alednaM nosleN =>", reverseString("Nelson Mandela") === "alednaM nosleN");
-
-console.log(reverseString("life is like a box of chocolates"), " <= setalocohc fo xob a ekil si efil =>", reverseString("life is like a box of chocolates") === "setalocohc fo xob a ekil si efil");
-
-console.log(reverseString("huzzah"), " <= hazzuh =>", reverseString("huzzah") === "hazzuh");
+// console.log(reverseString("Nelson Mandela"), " <= alednaM nosleN =>", reverseString("Nelson Mandela") === "alednaM nosleN");
+//
+// console.log(reverseString("life is like a box of chocolates"), " <= setalocohc fo xob a ekil si efil =>", reverseString("life is like a box of chocolates") === "setalocohc fo xob a ekil si efil");
+//
+// console.log(reverseString("huzzah"), " <= hazzuh =>", reverseString("huzzah") === "hazzuh");
 
 console.log(); // for spacing
